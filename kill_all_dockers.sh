b822370089c9e19d33efbbb8157eb1670a0b6b54
Paul Artushkov <pavTiger@gmail.com>
2022-12-04 08:46:03 +0000

    Fortify kill_all_dockers script with a prompt

--- kill_all_dockers.sh
+++ kill_all_dockers.sh
@@ -1,3 +1,13 @@
-docker kill $(docker ps -q)
-docker rm $(docker ps --filter status=created -q)
-docker rm $(docker ps --filter status=exited -q)
+stop_and_kill () {
+	docker kill $(docker ps -q)
+	docker rm $(docker ps --filter status=created -q)
+	docker rm $(docker ps --filter status=exited -q)
+}
+
+read -p "Do you wish to stop and delete ALL containers? y/N " answer
+case $answer in
+		[Yy]* ) stop_and_kill; break;;
+		[Nn]* ) exit;;
+		* ) exit;;
+esac
+
